f3301b4bd23a772246074ce6fa55252a1ee0f78d
Andreas <antmanos42@gmail.com>
2026-04-19 14:50:37 +0200

    fixed the hydration mismatch

--- web/src/components/ui/Input.tsx
+++ web/src/components/ui/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useId } from 'react';
 import './Input.css';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -7,7 +7,8 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 export default function Input({ label, required, className, id, ...props }: InputProps) {
-  const inputId = id || `input-${Math.random().toString(36).substr(2, 9)}`;
+  const generatedId = useId();
+  const inputId = id || generatedId;
   
   return (
     <div className={`ui-input-wrapper ${className || ''}`}>
